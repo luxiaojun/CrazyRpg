99fb009968db8c84336cfcdd08a54e0d336fb940
luxiaojun <luxiaojun@luxiaojuntekiMac-mini.local>
2013-06-27 23:41:14 +0800

    Initial Commit

---
+++ CrazyRpg/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  CrazyRpgAppController.h
+//  CrazyRpg
+//
+//  Created by luxiaojun on 13-6-27.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ CrazyRpg/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  CrazyRpgAppController.h
+//  CrazyRpg
+//
+//  Created by luxiaojun on 13-6-27.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
